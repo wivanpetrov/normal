87cede3bfec2d7aa0a56c0d90f61d4d42a76bbe4
wivanpetrov <wivan_petrov_70@mail.ru>
2024-08-17 00:26:38 +0300

    Update stop.sh
    the lines to remove app's Docker network is added

--- stop.sh
+++ stop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# The section to define script's vars
+# enter here the same name as for start.sh script
+network_name=normal01
+
 # to stop all the containers
+# they are started with --rm, we don't need to remove it
 docker stop $(docker ps -a -q)
+
+# to remove the app's network
+docker network rm $network_name
 
