22d5588f4f58b40558f15bb000f07b8287aa52fb
wivanpetrov <wivan_petrov_70@mail.ru>
2024-08-17 00:34:48 +0300

    Update stop.sh
    Echo lines after command lines to inform user  are added

--- stop.sh
+++ stop.sh
@@ -7,7 +7,8 @@ network_name=normal01
 # to stop all the containers
 # they are started with --rm, we don't need to remove it
 docker stop $(docker ps -a -q)
+echo "all containers are stopped and revoved!"
 
 # to remove the app's network
 docker network rm $network_name
-
+echo "network $network_name is removed!"
